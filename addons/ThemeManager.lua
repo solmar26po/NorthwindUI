7c4168e82a458ba42c5ded59d4306c61abcc292f
Solmar Felix <contributor60@gmail.com>
2026-08-26 22:51:13 -0700

    Add addons/ThemeManager.lua

---
+++ addons/ThemeManager.lua
@@ -0,0 +1,48 @@
+-- Northwind UI theme helper
+local ThemeManager = {
+    Library = nil,
+}
+
+function ThemeManager:SetLibrary(library)
+    self.Library = library
+    return self
+end
+
+function ThemeManager:RegisterTheme(name, palette)
+    assert(self.Library, "Call ThemeManager:SetLibrary first")
+    assert(type(name) == "string", "Theme name must be a string")
+    assert(type(palette) == "table", "Theme palette must be a table")
+
+    local base = table.clone(self.Library.Themes.Midnight)
+    for token, value in pairs(palette) do
+        base[token] = value
+    end
+    self.Library.Themes[name] = base
+    return self
+end
+
+function ThemeManager:ApplyTheme(name)
+    assert(self.Library, "Call ThemeManager:SetLibrary first")
+    self.Library:SetTheme(name)
+    return self
+end
+
+function ThemeManager:SetAccent(color)
+    assert(self.Library, "Call ThemeManager:SetLibrary first")
+    self.Library:SetAccent(color)
+    return self
+end
+
+function ThemeManager:GetThemes()
+    assert(self.Library, "Call ThemeManager:SetLibrary first")
+    local names = {}
+    for name in pairs(self.Library.Themes) do
+        table.insert(names, name)
+    end
+    table.sort(names)
+    return names
+end
+
+return ThemeManager
+
+
